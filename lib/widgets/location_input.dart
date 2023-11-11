719d9f50e3f915fffacbcd4754459b84656fe83e
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2023-11-11 16:35:40 +0530

    used location library to get user's current location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -8,8 +9,56 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  bool _isGettingLocation = false;
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+
+    setState(() {
+      _isGettingLocation = false;
+    });
+
+    print(locationData.latitude);
+    print(locationData.longitude);
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No location chosen',
+      style: Theme.of(context)
+          .textTheme
+          .bodyLarge!
+          .copyWith(color: Theme.of(context).colorScheme.onBackground),
+    );
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator();
+    }
     return Column(
       children: [
         Container(
@@ -21,20 +70,14 @@ class _LocationInputState extends State<LocationInput> {
                   width: 1,
                   color:
                       Theme.of(context).colorScheme.primary.withOpacity(0.2))),
-          child: Text(
-            'No location chosen',
-            style: Theme.of(context)
-                .textTheme
-                .bodyLarge!
-                .copyWith(color: Theme.of(context).colorScheme.onBackground),
-          ),
+          child: previewContent,
         ),
         const SizedBox(height: 12),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
               icon: const Icon(Icons.location_on),
               label: const Text('Get current location'),
             ),
